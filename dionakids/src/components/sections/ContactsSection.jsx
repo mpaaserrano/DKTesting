58eb127b80fbdfbd0bf2e3b299a8a5ac198e9024
mpaaserrano <miguel.paa.serrano@gmail.com>
2026-01-02 15:41:40 +0000

    Added FAQ accordions

--- dionakids/src/components/sections/ContactsSection.jsx
+++ dionakids/src/components/sections/ContactsSection.jsx
@@ -1,5 +1,7 @@
 import Grid from "../layout/Grid";
 import "./ContactsSection.css";
+import Accordion from "../accordion/Accordion"
+import AccordionItem from "../accordion/AccordionItem"
 
 const ContactsSection = () => {
     return (
@@ -11,7 +13,24 @@ const ContactsSection = () => {
 
             {/* CONTENT */}
             <div className="contacts-content">
-                
+                <Accordion>
+                    <AccordionItem title={"Qual a idade recomendada?"}>
+                        4-99 anos.
+                    </AccordionItem>
+
+                    <AccordionItem title={"É necessário experiência própria?"}>
+                        Não.
+                    </AccordionItem>
+
+                    <AccordionItem title={"Quais são os horários?"}>
+                        24/7
+                    </AccordionItem>
+
+                    <AccordionItem title={"Existe opção online?"}>
+                        Sim.
+                    </AccordionItem>
+
+                </Accordion>
 
             </div>
         </section>
